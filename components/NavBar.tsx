dedce566353d04b77520eec480d228fadb61457d
Quintin720 <lisaon75@gmail.com>
2025-05-22 08:21:11 +0100

    add _app.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,1 +1,14 @@
-export default function NavBar() { return <nav>NavBar</nav>; }
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav style={{ padding: '1rem', backgroundColor: '#f8f8f8', borderBottom: '1px solid #ccc' }}>
+      <a href="/" style={{ marginRight: '1rem' }}>Home</a>
+      <a href="/about" style={{ marginRight: '1rem' }}>About</a>
+      <a href="/courses" style={{ marginRight: '1rem' }}>Courses</a>
+      <a href="/blog">Blog</a>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import type { AppProps } from 'next/app';
+import NavBar from '../components/NavBar';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
